1b5b7c84ab2427ad8c90f5a071d089bca1760b06
Yash Rathod <yashvrathod15@gmail.com>
2025-10-05 19:19:12 +0530

    final5

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,7 +8,7 @@ declare global {
 export const prisma =
   global.prisma ??
   new PrismaClient({
-    log: ["query", "error", "warn", "info"],
+    log: ["query", "warn", "error", "info"],
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
