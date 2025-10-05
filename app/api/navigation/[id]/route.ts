0e145930fd201dceb15d894b584438a8b12534ac
Yash Rathod <yashvrathod15@gmail.com>
2025-10-05 19:15:46 +0530

    final-prisma1234

--- app/api/navigation/[id]/route.ts
+++ app/api/navigation/[id]/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server"
 import { prisma } from "@/lib/prisma"
 
+export const dynamic = "force-dynamic"
+
 export async function PUT(request: Request, { params }: { params: { id: string } }) {
   try {
     const body = await request.json()
